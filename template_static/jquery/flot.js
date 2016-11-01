b837e8cdb5dd3e33d00382c46bcf75ba74472f46
Jesse Rengers <jessejanrengers@gmail.com>
2016-11-01 14:50:01 +0100

    include wordcloud

--- template_static/jquery/flot.js
+++ template_static/jquery/flot.js
@@ -14,4 +14,15 @@ $(document).ready(function(){
             }
         }
     });
+    
+    block('#wolk').wordcloud({
+        //Applying logarithmic filter for rare hashtags
+        filter_function: function(cat, val, max) {
+            return Math.pow(val, (1/Math.log10(max))) > 1.8
+            },
+        //Calculating logarithmic weightfactor from 1 to 10
+        weight_function: function(cat, val, max) {
+            return Math.round(Math.pow(val, (1/Math.log10(max))))
+        }
+    });
 });
