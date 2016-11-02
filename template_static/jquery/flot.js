26dcbaded60250b6d2efccf90afc552a091363d3
remco1112 <remco1112@gmail.com>
2016-11-02 15:41:08 +0100

    Added documentation

--- template_static/jquery/flot.js
+++ template_static/jquery/flot.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    //Assigning the blocks to the divs
     block('#tweetcounter').rolling_chart({
         memory: 75,
         series: {
@@ -14,9 +15,8 @@ $(document).ready(function(){
             }
         }
     });
-    
     block('#wolk').wordcloud({
-        //Applying logarithmic filter for rare hashtags
+        //Applying logarithmic filter to remove rare hashtags
         filter_function: function(cat, val, max) {
             return Math.pow(val, (1/Math.log10(max))) > 1.8
             },
@@ -33,6 +33,7 @@ $(document).ready(function(){
     });
     block('#map').map({})
 });
+//Initialize map
 var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {zoom: 9, center: {lat: 51.99780, lng: 6.44820}, mapTypeId: 'roadmap'});
