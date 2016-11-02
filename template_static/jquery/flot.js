6759fa9b301607ea6385a32eed9905e43fd22df3
remco1112 <remco1112@gmail.com>
2016-11-02 15:09:28 +0100

    Added map

--- template_static/jquery/flot.js
+++ template_static/jquery/flot.js
@@ -31,53 +31,9 @@ $(document).ready(function(){
     block('#populartweetfeed').tweets({
     memory: 3
     });
+    block('#map').map({})
 });
+var map;
 function initMap() {
-    var map;
     map = new google.maps.Map(document.getElementById('map'), {zoom: 9, center: {lat: 51.99780, lng: 6.44820}, mapTypeId: 'roadmap'});
 }
-(function($, block) {
-
-block.fn.map = function(config) {
-    // handle configuration
-    var options = $.extend({
-        size: '64pt',
-        text: 'RED',
-        color: 'red'
-    }, config);
-
-    // create HTML representation
-    var $el = $('<span></span>').appendTo(this.$element);
-    $el.css('font-size', options.size);
-
-    // create HTML element for display
-    var data = {
-        text: options.text,
-        color: options.color
-    }
-
-    // update function to update element
-    var update = function() {
-        $el.text(data.text+'!').css('color', data.color);
-    }
-
-    // invoke update to initialise the display
-    update();
-
-    // register actions
-    this.actions({
-        word: function(e, message) {
-            data.text = message.text;
-            update();
-        },
-        color: function(e, message) {
-            data.color = message.color;
-            update();
-        }
-    });
-
-    // return the element for further work
-    return this.$element;
-}
-
-})(jQuery, block);
