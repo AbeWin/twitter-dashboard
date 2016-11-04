6ba86c09d9b8f671867f51813a92d6cf90e123c3
StijnBerendse <s.berendse@live.nl>
2016-11-04 15:56:11 +0100

    Added documentation

--- template.py
+++ template.py
@@ -22,15 +22,19 @@ def checktime(ctx, e):
     if tweet_count == 0:
         tweet_count=0.01
     emit('tweetcount',{'action': 'add', 'value': tweet_count})
+    #Reset tweet count for next iteration
     tweet_count=0
     fire('timer', delay=1)
+#For every tweet add the hashtags to the wordcloud with value 1
 def updateWordCloud(tweetdata):
     hashtags = tweetdata['entities']['hashtags']
     for i in range(len(hashtags)):
         emit('word', {'action': 'add', 'value': [hashtags[i]["text"], 1]})
+#For every tweet send, if coordinates exist, coordinates and tweet text to tweetmap
 def updateMap(tweetdata):
     if tweetdata['coordinates'] != None:
         emit('map', {'action': 'add', 'values': [tweetdata['coordinates']['coordinates'], tweetdata["text"]]})
+#Emit tweet if user matches @batavierenrace
 def updateOfficialTweet(tweetdata):
     if tweetdata['user']['id_str'] == "19373068":
         emit('officialtweet', tweetdata)
@@ -39,8 +43,11 @@ def updateOfficialTweet(tweetdata):
 def tweet(ctx, e):
     global tweet_count
     tweet_count = tweet_count + 1
+    #Retrieve tweet data from e
     tweetdata = e.data
+    #Call update functions for wordcloud, map, official tweetfeed
     updateWordCloud(tweetdata)
     updateMap(tweetdata)
-    emit('tweet', tweetdata)
     updateOfficialTweet(tweetdata)
+    #update live tweet feed
+    emit('tweet', tweetdata)
