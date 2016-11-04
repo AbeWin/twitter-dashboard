a9060a7e3a1dbe218c15bb3c07fdcac0b0ca190a
StijnBerendse <s.berendse@live.nl>
2016-11-04 16:07:46 +0100

    Added documentation

--- template_static/jquery/flot.js
+++ template_static/jquery/flot.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    //Initialise tweetometer gauge
     initGauge();
     block('#tweetcounter').rolling_chart({
         memory: 75,
@@ -17,7 +18,7 @@ $(document).ready(function(){
     });
     
     block('#wolk').wordcloud({
-        //Applying logarithmic filter for rare hashtags
+        //Applying logarithmic filter to delete rarely used hashtags
         filter_function: function(cat, val, max) {
             return Math.pow(val, (1/Math.log10(max))) > 1.8
             },
@@ -35,10 +36,12 @@ $(document).ready(function(){
     block('#map').map({});
     block('#gauge').gauge({});
 });
+//Initialise maP (done by google) with correct zoom and center
 var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {zoom: 9, center: {lat: 51.99780, lng: 6.44820}, mapTypeId: 'roadmap'});
 }
+//initialise tweetometer gauge with correct scale
 var gauge;
 function initGauge() {
     var max = 80;
